a4fef6aa582ee0d593575ff2a08a8d81456188d9
magihoefkens <magi.hoefkens@outlook.de>
2024-01-17 16:05:35 +0100

    Fronted changed for add, update and delete Instructor.

---
+++ frontend/src/components/NewInstructorForm.tsx
@@ -0,0 +1,150 @@
+import {InstructorDto} from "../models/InstructorDto.ts";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormHelperText,
+    FormLabel,
+    TextField
+} from "@mui/material";
+import Swal from "sweetalert2";
+import {Qualification} from "../models/Qualification.ts";
+
+type AddInstructorProps={
+    addInstructor:(instructor:InstructorDto) =>void;
+    closeDialog:() =>void;
+}
+export default function NewInstructorForm(props:AddInstructorProps){
+     const addInstructor = props.addInstructor;
+
+     const[qualification,setQualification]=useState<Qualification>({
+         isSkiInstructor: false,
+         isSnowboardInstructor: false,
+         isNordicSkiInstructor: false,
+         isSegwayInstructor: false,
+         isHikingGuide: false
+     });
+    const[instructor,setInstructor]=useState<InstructorDto>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        qualification
+    });
+    function handleChangeName(event: ChangeEvent<HTMLInputElement>) {
+        setInstructor({...instructor, [event.target.name]: event.target.value})
+    }
+    function handleSubmitNewInstructor(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        props.closeDialog();
+        Swal.fire({
+            title: 'Hinzufügen...',
+            text: 'Bitte warten...',
+            didOpen() {
+                Swal.showLoading()
+                addInstructor(instructor);
+            }
+        }).then();
+        setInstructor({
+            firstName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            qualification: {
+                isSkiInstructor: false,
+                isSnowboardInstructor: false,
+                isNordicSkiInstructor: false,
+                isSegwayInstructor: false,
+                isHikingGuide: false,
+            }});
+    }
+
+    function handleChangeCheckbox(event:ChangeEvent<HTMLInputElement>) {
+        const {name,checked}=event.target;
+        setQualification(
+            (prevQualification) => ({
+                ...prevQualification,
+                [name]:checked,
+            })
+        );
+        setInstructor(
+            (prevInstructor) => ({
+                ...prevInstructor,
+                qualification: {
+                    ...prevInstructor.qualification,
+                    [name]:checked,
+                },
+            })
+        );
+        console.log(instructor)
+    }
+
+    return(
+         <Box sx={{minWidth: 120, mt: 2}} component={"form"} onSubmit={handleSubmitNewInstructor}>
+             <FormControl fullWidth sx={{gap: 2}}>
+
+                 <TextField value={instructor.firstName}
+                            name="firstName"
+                            onChange={handleChangeName}
+                            label="Vorname"
+                            variant="outlined"/>
+                 <TextField value={instructor.lastName}
+                            name="lastName"
+                            onChange={handleChangeName}
+                            label="Nachname"
+                            variant="outlined"/>
+                 <TextField value={instructor.phoneNumber}
+                            name="phoneNumber"
+                            onChange={handleChangeName}
+                            label="Telefon"
+                            variant="outlined"/>
+                 <TextField value={instructor.email}
+                            name="email"
+                            onChange={handleChangeName}
+                            label="Email"
+                            variant="outlined"/>
+                 <FormLabel component="legend">Qualifikationen zuweisen</FormLabel>
+                 <FormGroup>
+                     <FormControlLabel
+                         control={
+                             <Checkbox checked={qualification.isSkiInstructor} onChange={handleChangeCheckbox} name="isSkiInstructor" />
+                         }
+                         label="Skilehrer"
+                     />
+                     <FormControlLabel
+                         control={
+                             <Checkbox checked={qualification.isSnowboardInstructor} onChange={handleChangeCheckbox} name="isSnowboardInstructor" />
+                         }
+                         label="Snowboardlehrer"
+                     />
+                     <FormControlLabel
+                         control={
+                             <Checkbox checked={qualification.isNordicSkiInstructor} onChange={handleChangeCheckbox} name="isNordicSkiInstructor" />
+                         }
+                         label="Nordic SkiLehrer"
+                     />
+                     <FormControlLabel
+                         control={
+                             <Checkbox checked={qualification.isSegwayInstructor} onChange={handleChangeCheckbox} name="isSegwayInstructor" />
+                         }
+                         label="Segwaylehrer"
+                     />
+                     <FormControlLabel
+                         control={
+                             <Checkbox checked={qualification.isHikingGuide} onChange={handleChangeCheckbox} name="isHikingGuide" />
+                         }
+                         label="Wanderführer"
+                     />
+                 </FormGroup>
+                 <FormHelperText>Wählen Sie die Qualifikationen des Lehrer aus</FormHelperText>
+
+
+                 <Button type={"submit"}>Hinzufügen</Button>
+             </FormControl>
+         </Box>
+     );
+     }
---
+++ frontend/src/hooks/useInstructors.tsx
@@ -0,0 +1,95 @@
+import {useCallback, useEffect, useState} from "react";
+import {Instructor} from "../models/Instructor.ts";
+import Swal from "sweetalert2";
+import axios from "axios";
+import ErrorHandling from "../components/errorHandling/ErrorHandling.tsx";
+import {InstructorDto} from "../models/InstructorDto.ts";
+import {toast} from "react-toastify";
+
+export default function useInstructors(){
+    const [instructors,setInstructors]=useState<Instructor[]>([])
+    const[loading,setLoading]=useState<boolean>(false);
+    const BASE_URL_INSTRUCTORS:string='api/instructors';
+    const fetchInstructors = useCallback(() => {
+        setLoading(true);
+        Swal.fire({
+            title: 'Skilehrer werden geladen...',
+            didOpen: () => {
+                Swal.showLoading()
+                axios.get(BASE_URL_INSTRUCTORS)
+                    .then(r => {
+                        setInstructors(r.data);
+                        Swal.close();
+                    })
+                    .catch(() => <ErrorHandling message={"Fehler beim Laden der Skilehrer"}/>)
+                    .finally(() => setLoading(false));
+            }
+        }).then();
+    }, []);
+
+    useEffect(() => {
+        fetchInstructors();
+    }, [fetchInstructors]);
+
+    function addInstructor(instructor:InstructorDto) {
+        setLoading(true);
+        Swal.fire({
+            title: 'Skilehrer wird hinzugefügt...',
+            didOpen: () => {
+                Swal.showLoading();
+                    axios.post(BASE_URL_INSTRUCTORS, instructor)
+                        .then((r) => {
+                            Swal.close();
+                            fetchInstructors();
+                            toast.success("Skilehrer " + r.data.firstName + " " + r.data.lastName + " erfolgreich hinzugefügt");
+                        })
+                        .catch(() => <ErrorHandling message={"Fehler beim Hinzufügen des Dozenten"}/>)
+                        .finally(() => setLoading(false));
+                }
+            }
+        ).then();
+    }
+
+    function updateInstructor(id: string, instructor: InstructorDto) {
+        setLoading(true);
+        Swal.fire({
+            title: 'Skilehrer wird aktualisiert...',
+            didOpen: () => {
+                Swal.showLoading();
+
+                    axios.put(BASE_URL_INSTRUCTORS + '/' + id, instructor)
+                        .then(() => {
+                            Swal.close();
+                            fetchInstructors();
+                            toast.success("Skilehrer wurde aktualisiert");
+                        })
+                        .catch(() => <ErrorHandling message={"Fehler beim Aktualisieren des Skilehrer"}/>)
+                        .finally(() => setLoading(false));
+
+            }
+        }).then();
+    }
+
+    function deleteInstructor(id: string) {
+        setLoading(true);
+        Swal.fire({
+            title: 'Skilehrer wird gelöscht...',
+            didOpen: () => {
+                Swal.showLoading();
+
+                    axios.delete(BASE_URL_INSTRUCTORS + '/' + id)
+                        .then(() => {
+                            Swal.close();
+                            fetchInstructors();
+                            toast.success("Dozent erfolgreich gelöscht");
+                        })
+                        .catch(() => <ErrorHandling message={"Fehler beim Löschen des Skilehrers"}/>)
+                        .finally(() => setLoading(false));
+                }
+
+        }).then();
+    }
+
+    return {instructors, loading, addInstructor, updateInstructor, deleteInstructor};
+
+}
